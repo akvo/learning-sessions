1da84ca31e8b48c78d50ca39a610bc63aaa46897
Daniel Sunnerek <daniel@kardans.com>
2020-11-05 11:51:12 +0100

    Posting to Zulip

--- github-to-zulip/pull-reminders.sh
+++ github-to-zulip/pull-reminders.sh
@@ -2,13 +2,22 @@
 
 set -euo pipefail
 
-github_pull() {
-# curl -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/repos/akvo/akvo-lumen/pulls?state=open" > /tmp/github-pulls.json
-
-}
+#github_pull() {
+#    curl -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/repos/akvo/akvo-lumen/pulls?state=open" > /tmp/github-pulls.json
+#}
 
 list_open_pull_requests() {
-open_prs="$(jq .[].html_url /tmp/github-pulls.json)"
+    open_prs="$(jq -r .[].html_url github-pulls.json)"
+    echo $open_prs
 }
 
-post_to_zulip(){}
+post_to_zulip(){
+    curl -X POST https://akvo.zulipchat.com/api/v1/messages \
+         -u "${ZULIP_TOKEN}" \
+         -d 'type=stream' \
+         -d 'to=flumen-dev' \
+         -d 'topic=Pull reminder' \
+         -d "content=$1"
+}
+
+post_to_zulip "$(list_open_pull_requests)"
