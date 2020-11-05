4d1b833490920f87e3630e342008cc58071b31ed
Emmanuel Mulo <emmanuel@akvo.org>
2020-11-05 11:34:54 +0100

    List PR Urls

--- github-to-zulip/pull-reminders.sh
+++ github-to-zulip/pull-reminders.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
-github_pull() {}
+github_pull() {
+# curl -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/repos/akvo/akvo-lumen/pulls?state=open" > /tmp/github-pulls.json
 
-list_open_pull_requests() {}
+}
+
+list_open_pull_requests() {
+open_prs="$(jq .[].html_url /tmp/github-pulls.json)"
+}
 
 post_to_zulip(){}
